adce7efd3930db8aa60f0bdf4f18436058abf0a4
BogdanMaria <>
2023-11-11 10:00:02 +0000

    Created the game function. Ask for user input. Check if the guess is wright or wrong and let the user know. Set a number of maximum attempts.

--- run.py
+++ run.py
@@ -12,4 +12,35 @@ def get_guess():
     Ask for a letter input from the user and make it lower case
     """
     return input("Enter a letter: ").lower()
-get_guess()
+
+def play_hangman():
+    """
+    Set up the game with allowed number of incorrect guesses.
+    Check if the guess is right or wrong and let the user know.
+    Check if the player has guessed all the letters or if the attempts have been reached.
+    """
+    word_to_guess = choose_word()
+    guessed_letters = []
+    attempts = 6
+
+    print("Welcome to Hangman!")
+
+    while attempts > 0:
+        guess = get_guess()
+
+        if guess in word_to_guess:
+            print("Good guess!")
+            guessed_letters.append(guess)
+        else:
+            print("Incorrect guess!")
+            attempts -= 1
+        
+        if all(letter in guessed_letters for letter in word_to_guess):
+            print("Congratulations! You guessed the word:", word_to_guess)
+            break
+
+    if attempts == 0:
+        print("Sorry, you ran out of attempts. The word was:", word_to_guess)
+
+if __name__ == "__main__":
+    play_hangman()
