a184585c5b2dd6977c5cc9fee233b35d09a0d3b0
BogdanMaria <>
2023-11-11 09:33:17 +0000

    Create a function that selects a random word from a list

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def choose_word() :
+    """
+    Chooses a random word from a given list of words
+    """
+    words = ["programming", "function", "keyboard", "python", "hangman"]
+    print(random.choice(words))
+choose_word()
