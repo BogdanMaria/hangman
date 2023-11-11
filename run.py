153e48327df70862985cedc5bb9ef01ba73068e6
BogdanMaria <>
2023-11-11 09:41:22 +0000

    Ask the user for a letter input

--- run.py
+++ run.py
@@ -5,5 +5,11 @@ def choose_word() :
     Chooses a random word from a given list of words
     """
     words = ["programming", "function", "keyboard", "python", "hangman"]
-    print(random.choice(words))
-choose_word()
+    return random.choice(words)
+
+def get_guess():
+    """
+    Ask for a letter input from the user and make it lower case
+    """
+    return input("Enter a letter: ").lower()
+get_guess()
